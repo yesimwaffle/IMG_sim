78b09bf8315faeb48f38178f1d0b98c43237473a
waffle <ckak205@gmail.com>
2020-07-24 22:34:42 +0900

    make first file_re

--- git_1py.py
+++ git_1py.py
@@ -1,4 +1,7 @@
-if 1 >= 2:
-    print("fuck")
-elif 1 <= 2:
-    print("hello world")
+import cv2
+a = "/Users/waffle/Documents/random pic/70559da1-e676-49.jpg"
+cv2.imread(a)
+b = cv2.imread(a)
+
+b = cv2.cvtColor(b, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('b.png', b)
